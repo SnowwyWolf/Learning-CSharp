8f6c0c062900f4cf79472adc347ff2cc229d01ad
SnowwyWolf <hysteriasc2@hotmail.com>
2016-08-01 08:40:03 +0100

    Examples of operators
    Usage of basic operations.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
-namespace Hello_World
+namespace Operators_3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");       
+            var a = 1;
+            var b = 2;
+            Console.WriteLine(a > b); // Gives a result of either true or false.
+
+            var c = 3;
+            Console.WriteLine(c > b && c > a); // If both C is greater than a and b the result is true
         }
     }
 }
