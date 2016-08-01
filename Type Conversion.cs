27d903e20595ada3fc269fe6789ebe1d31dff4d5
SnowwyWolf <hysteriasc2@hotmail.com>
2016-08-01 08:48:45 +0100

    Type conversion
    Very basic type conversions using try and catch.

---
+++ Type Conversion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Type_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1000;
+            byte b = (byte)i;
+            Console.WriteLine(b);
+            try
+            {
+                var number = "1234";
+                int a = Convert.ToByte(number);
+                Console.WriteLine(a);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte");
+                
+            }
+            try
+            {
+                string str = "true";
+                bool f = Convert.ToBoolean(str);
+                Console.WriteLine(f);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
