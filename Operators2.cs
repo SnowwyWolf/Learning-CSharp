2a7bdeb01dbbbc9fcb9b380c62be80228c3a1547
SnowwyWolf <hysteriasc2@hotmail.com>
2016-08-01 08:46:41 +0100

    Usage of operators.
    Some very basic coding examples for the usage of operators.

---
+++ Operators2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Operators_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+            Console.WriteLine(a + b * c); // equates to b * c + a
+        }
+    }
+}
---
+++ Operators3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Operators_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            Console.WriteLine(a > b); // Gives a result of either true or false.
+
+            var c = 3;
+            Console.WriteLine(c > b && c > a); // If both C is greater than a and b the result is true
+        }
+    }
+}
