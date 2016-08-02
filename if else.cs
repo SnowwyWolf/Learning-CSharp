abeb75f82316d047a1ebed4e030c14ab60bf60cc
SnowwyWolf <hysteriasc2@hotmail.com>
2016-08-02 09:12:54 +0100

    Add files via upload

---
+++ if else.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+
+namespace if_else_switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's Morning");
+            }
+
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's afternoon");
+            }
+            else
+            {
+                Console.WriteLine("It's evening");
+            }
+            
+        }
+    }
+}
+    
+
