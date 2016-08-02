80a86873e9e48c683e002eaca0a319f22eeb86c6
SnowwyWolf <hysteriasc2@hotmail.com>
2016-08-02 17:26:36 +0100

    Add files via upload

---
+++ Exercises/Exercise 4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the speedlimit of the area: ");
+            var speedLimit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the speed of the car");
+            var carSpeed = Convert.ToInt32(Console.ReadLine());
+
+            if (carSpeed <= speedLimit) 
+            {
+                Console.WriteLine("Ok");
+            }
+            else
+            {
+                const int kmPerDemeritPoint = 5;
+                var demeritPoints = (carSpeed - speedLimit)/kmPerDemeritPoint;
+                if (demeritPoints > 12)
+                
+                    Console.WriteLine("License Suspended");
+                
+                else
+                
+                    Console.WriteLine("Demerit Points: " + demeritPoints);
+                
+            }
+        
+        }
+    }
+}
