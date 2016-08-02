4f63cb2e8d820db0fcf387b9a32fe4352585c2cf
SnowwyWolf <hysteriasc2@hotmail.com>
2016-08-02 10:18:09 +0100

    Add files via upload

---
+++ Exercises/Exercise 2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first number");
+            var firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your second number");
+            var secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine(firstNumber);
+            }
+            else
+            {
+                Console.WriteLine(secondNumber);
+            }
+            
+        }
+    }
+}
