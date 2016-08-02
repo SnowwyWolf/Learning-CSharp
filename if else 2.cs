7b6c65ce48dee3559a8847c4b843248ad1747150
SnowwyWolf <hysteriasc2@hotmail.com>
2016-08-02 09:20:16 +0100

    Add files via upload

---
+++ if else 2.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System;
+
+namespace if_else_switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isGoldCustomer = true;
+
+              //   float price;
+             //    if (isGoldCustomer)
+             //     price = 19.95f;
+             //  else
+            //    price = 29.95f;
+
+            float price = (isGoldCustomer) ? 19.95f : 29.95f;
+
+            Console.WriteLine(price);
+        }
+    }
+}
+    
+
