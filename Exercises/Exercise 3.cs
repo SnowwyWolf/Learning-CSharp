d94b20b0b97adf980059929296dd67c4097fd030
SnowwyWolf <hysteriasc2@hotmail.com>
2016-08-02 10:27:16 +0100

    Add files via upload

---
+++ Exercises/Exercise 3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the height of your image");
+            var height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the width of your image");
+            var width = Convert.ToInt32(Console.ReadLine());
+
+            if (height > width)
+            {
+                Console.WriteLine("Your image is portrait");
+            }
+            else
+            {
+                Console.WriteLine("Your image is landscape");
+            }
+        }
+    }
+}
