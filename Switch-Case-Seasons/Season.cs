3b9b5afbc10f67491981d0428ac9b614248f0b8f
SnowwyWolf <hysteriasc2@hotmail.com>
2016-08-02 09:47:40 +0100

    Add files via upload

---
+++ Switch-Case-Seasons/Season.cs
@@ -0,0 +1,10 @@
+﻿namespace if_else_switch_case
+{
+    public enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+}
---
+++ Switch-Case-Seasons/Switch Case Promo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace if_else_switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var season = Season.Autumn;
+
+            switch (season)
+            {
+                case Season.Autumn:
+                case Season.Summer:
+                    Console.WriteLine("We've got promotions");
+                    break;
+
+                case Season.Winter:
+                    Console.WriteLine("It's snowwing, the cold is here");
+                    break;
+
+                default:
+                    Console.WriteLine("I don't understand that season");
+                    break;
+            }
+        }
+    }
+}
