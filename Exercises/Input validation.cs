4cefaf2036295546f6de03ac30d55e068e90c50d
SnowwyWolf <hysteriasc2@hotmail.com>
2016-08-02 10:07:26 +0100

    Add files via upload

---
+++ Exercises/Input validation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Input_validation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number between 1 to 10");
+            int userInput = 0;
+            userInput = int.Parse(Console.ReadLine());
+
+            if (userInput >= 1 && userInput <= 10)
+            {
+                Console.WriteLine("Valid Input");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+            
+        }
+    }
+}
