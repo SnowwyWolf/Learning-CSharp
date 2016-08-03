9e862bf853e6e74f4cf51ab6c640b70fe13c9b9c
SnowwyWolf <hysteriasc2@hotmail.com>
2016-08-03 16:10:54 +0100

    Add files via upload

---
+++ eBook/Variables Everywhere.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_Everywhere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5000000;
+            uint b = 20000000;
+            float c = 20.5050f;
+            double d = 30.586758854;
+            byte e = 250;
+            decimal f =  20.12545634M;
+            char g = 'g';
+            string h = "Eat the cake!";
+            long i = 135753784585478587L;
+            short j = 12346;
+            sbyte k = 50;
+            ulong l = 53453453456345;
+            ushort m = 1234;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+            Console.WriteLine(l);
+            Console.WriteLine(m);
+
+        }
+    }
+}
